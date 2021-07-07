34f0e0dd452be2c797c5cac3329b142188094615
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-07-07 13:57:36 +0200

    Init android-room-database project

---
+++ android-room-database/app/src/main/java/blog/mateuszgrabarski/database/MainActivity.kt
@@ -0,0 +1,16 @@
+package blog.mateuszgrabarski.database
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import blog.mateuszgrabarski.database.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
